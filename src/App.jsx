2a009fa573f17d7d31f0742cb805c29d25d8ab8c
krishna pithiya <129658825+KRISHNAPITHIYA@users.noreply.github.com>
2024-05-27 07:39:35 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return <center class= 'todo-container'>
+    <h1>todo app</h1>
+    <div class="container text-center">
+  <div class="row">
+    <div class="col-4">col-8</div>
+    <div class="col-4">col-8</div>
+    <div class="col-2">col-4</div>
+  </div>
+</div>
+  </center>
+    
+}
+export default App
